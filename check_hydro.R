92e0029040847e130c9c946b20caa4bdf0305f9f
Porobic Garate, Javier Porobic (Environment, Hobart) <Javier.Porobicgarate@csiro.au>
2023-11-28 10:56:43 +1100

    added R-code to check the hydro output

---
+++ check_hydro.R
@@ -0,0 +1,25 @@
+library("shiny")
+library("DT")
+library("ggplot2")
+library("ncdf4")
+library("stringr")
+library('tidyverse')
+library("dplyr")
+library(shinyrAtlantis)
+
+
+exchange.file    <- 'Creating_Hydro/out_EAAM/EAAM_hydro.nc'
+salinity.file    <- "Creating_Hydro/out_EAAM/EAAM_salt.nc"       # this file is not included in the package
+temperature.file <- "Creating_Hydro/out_EAAM/EAAM_temp.nc"       # this file is not included in the package
+bgm.file         <- "Creating_Hydro/EAA29_ll_v2.bgm" # this file is not included in the package
+cum.depth = c(0, 20, 50, 100, 200, 300, 400, 750, 1000, 2000, 5000)
+
+input.object <- make.sh.forcings.object(
+  bgm.file         = bgm.file,
+  exchange.file    = exchange.file,
+  cum.depth        = cum.depth,
+  temperature.file = temperature.file,
+  salinity.file    = salinity.file
+)
+
+sh.forcings(input.object)
