40ff80a73e4949eb88e4dddd99c7bc9acafa5251
Porobic Garate, Javier Porobic (Environment, Hobart) <Javier.Porobicgarate@csiro.au>
2023-12-06 13:29:45 +1100

    fixed coordinated misplacement

--- sample_netcdf.py
+++ sample_netcdf.py
@@ -1,6 +1,5 @@
 import xarray as xr
 import numpy as np
-import geopandas as gpd
 import sys
 
 # getting the file name from the command line
@@ -9,10 +8,12 @@ variable = sys.argv[2]
 out_name = filename[-7:]
 # Now you can use the filename to open the dataset
 ds = xr.open_dataset(filename)
-
+# filename = '/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/ocean-3d-u-1-daily-mean-ym_1999_01.nc'
 # Open the netCDF file using xarray
-# ds = xr.open_dataset('temporal_input/ocean-3d-u-1-daily-mean-ym_1999_01.nc')
-
+# ds = xr.open_dataset(
+#    '/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/ocean-3d-u-1-daily-mean-ym_1999_01.nc')
+# out_name = filename[-7:]
+# variable = 'u'
 # Define the minimum and maximum longitude and latitude
 # These values are used to create a rectangle to zoom in on the region of interest (the EAAM)
 min_lon = 45.0
@@ -33,16 +34,15 @@ new_lon = np.where(temp_lon > 180, temp_lon - 360, temp_lon)
 sorted_indices = np.argsort(new_lon)
 # Create a new array for the sorted longitudes and the corresponding u values
 new_lon_sorted = new_lon[sorted_indices]
-print(new_lon_sorted)
+print(sorted_indices)
 u_sorted_reduced_list = []
 # Loop through all time and st_ocean values
 for t in range(ds.dims['time']):
     for d in range(ds.dims['st_ocean']):
-        # Extract one time step and one depth of the u array using isel
-        u = ds['u'].isel(time=t, st_ocean=d)
-        # Sort the u array based on the sorted indices of the longitude array
-        u_sorted = u[:, sorted_indices]
-        # print(u_sorted['xu_ocean'])
+        # Extract one time step and one depth of the u array
+        u_sorted = ds['u'].isel(time=t, st_ocean=d, xu_ocean=sorted_indices)
+        # Replace the 'xu_ocean' values in 'u_sorted' with 'new_lon_sorted'
+        u_sorted['xu_ocean'] = new_lon_sorted
         # Reduce the size of the u_sorted array based on the minimum and maximum longitude and latitude
         u_sorted_reduced = u_sorted.sel(xu_ocean=slice(
             min_lon, max_lon), yu_ocean=slice(min_lat, max_lat))
@@ -62,4 +62,8 @@ new_ds = xr.Dataset({'u': new_u}, coords={
                     'time': ds['time'], 'st_ocean': ds['st_ocean'], 'xu_ocean': new_lon_sorted, 'yu_ocean': ds['yu_ocean']})
 
 # Save the new Dataset to a netCDF file
-new_ds.to_netcdf('/datasets/work/oa-alantis/work/Hydro_EAA/sampled_outputs/' + variable + out_name, engine='h5netcdf')
+new_ds.to_netcdf('/datasets/work/oa-alantis/work/Hydro_EAA/sampled_outputs/' +
+                 variable + out_name, engine='h5netcdf')
+
+# new_ds.to_netcdf('/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/' +
+#                 variable + out_name, engine='h5netcdf')
