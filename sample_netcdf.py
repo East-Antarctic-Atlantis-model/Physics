4967fdb5057c97159522825cb0f6bee05d700a7e
Porobic Garate, Javier Porobic (Environment, Hobart) <Javier.Porobicgarate@csiro.au>
2023-12-13 09:17:03 +1100

    ready

--- sample_netcdf.py
+++ sample_netcdf.py
@@ -2,19 +2,14 @@ import xarray as xr
 import numpy as np
 import sys
 
+# filename = '/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/ocean-3d-v-1-daily-mean-ym_1999_01.nc'
 # getting the file name from the command line
-# filename = sys.argv[1]
-filename = '/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/ocean-3d-v-1-daily-mean-ym_1999_01.nc'
-# variable = sys.argv[2]
-out_name = filename[-7:]
+filename = sys.argv[1]
+variable = sys.argv[2]
+out_name = filename[-11:]
 # Now you can use the filename to open the dataset
 ds = xr.open_dataset(filename)
 
-# Open the netCDF file using xarray
-# ds = xr.open_dataset(
-#    '/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/ocean-3d-u-1-daily-mean-ym_1999_01.nc')
-# out_name = filename[-7:]
-variable = 'v'
 # Define the minimum and maximum longitude and latitude
 # These values are used to create a rectangle to zoom in on the region of interest (the EAAM)
 min_lon = 45.0
@@ -53,30 +48,21 @@ for t in range(ds.dims['time']):
         u_sorted_reduced_np = u_sorted_reduced.values
         u_sorted_reduced_array[t, d, :, :] = u_sorted_reduced_np
 
-# Convert the list of sorted and reduced u arrays to a 3D numpy array
-# u_sorted_reduced_array = np.array(u_sorted_reduced_list)
-# print out the dimension fo u_sorted_reduced_array
-print(u_sorted_reduced_array.shape)
-
 # Create a new 'xu_ocean' coordinate that matches the size of the 'xu_ocean' dimension in 'u_sorted_reduced_array'
 new_xu_ocean = new_lon_sorted[:u_sorted_reduced_array.shape[3]]
 new_yu_ocean = ds['yu_ocean'].sel(yu_ocean=slice(min_lat, max_lat))
 
 # Create a new xarray.DataArray with the new_u DataArray and the new coordinates
-# Create a new xarray.DataArray with the new_u DataArray and the new coordinates
 new_u = xr.DataArray(u_sorted_reduced_array, dims=ds[variable].dims, coords={
                      'time': ds['time'], 'st_ocean': ds['st_ocean'], 'xu_ocean': new_xu_ocean, 'yu_ocean': new_yu_ocean})
-# Create a new xarray.DataArray with the same dimensions and coordinates as the original u array
-# new_u = xr.DataArray(u_sorted_reduced_array,
-#                    dims=ds['u'].dims, coords=ds['u'].coords)
 
 # Create a new xarray.Dataset with the new_u DataArray and the new coordinates
 new_ds = xr.Dataset({variable: new_u}, coords={
                     'time': ds['time'], 'st_ocean': ds['st_ocean'], 'xu_ocean': new_xu_ocean, 'yu_ocean': new_yu_ocean})
 
 # Save the new Dataset to a netCDF file
-# new_ds.to_netcdf('/datasets/work/oa-alantis/work/Hydro_EAA/sampled_outputs/' +
-#                 variable + out_name, engine='h5netcdf')
-
-new_ds.to_netcdf('/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/' +
+new_ds.to_netcdf('/datasets/work/oa-alantis/work/Hydro_EAA/sampled_outputs/' +
                  variable + out_name, engine='h5netcdf')
+
+# new_ds.to_netcdf('/home/por07g/Documents/Projects/Supervision/Ilaria/tools/temporal_input/' +
+#                  variable + out_name, engine='h5netcdf')
